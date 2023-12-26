b400b8ecf47e246dfd0d33e7bb8b9e9801219ce7
lucateodorescu <lvteodorescu@gmail.com>
2023-12-26 19:51:56 +0100

    add: agents.py to implement players

---
+++ agents.py
@@ -0,0 +1,16 @@
+# Code for the implementation of agents playing chess. Current implementation is done with the chess library, but this will be changed to use the game.py file in the future.
+
+import chess
+
+class Agent:
+    # Agent class to represent an agent playing chess
+
+    def __init__(self, color):
+        # Initialize an agent
+
+        self.color = color
+
+    def move(self, board):
+        # Move a piece on the board
+        return None
+    
